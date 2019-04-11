6c25a0fd34b2487b20e59398b37328a2632a7bd7
n8steiner <n8steiner98@gmail.com>
2019-04-11 14:03:00 -0400

    through step 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190409173538) do
+ActiveRecord::Schema.define(version: 20190411175701) do
+
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
