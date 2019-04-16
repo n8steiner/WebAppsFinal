9b39e48b06216ed7c853c413efd9370464d0fb70
n8steiner <n8steiner98@gmail.com>
2019-04-16 14:56:23 -0400

    through step 17

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,10 +1,11 @@
 class PlansController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_plan, only: [:show, :edit, :update, :destroy]
 
   # GET /plans
   # GET /plans.json
   def index
-    @plans = Plan.all
+    @plans = Plan.where(user_id: current_user.id)
   end
 
   # GET /plans/1
@@ -25,6 +26,7 @@ class PlansController < ApplicationController
   # POST /plans.json
   def create
     @plan = Plan.new(plan_params)
+    @plan.user_id = current_user.id
 
     respond_to do |format|
       if @plan.save
@@ -69,6 +71,6 @@ class PlansController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def plan_params
-      params.require(:plan).permit(:name)
+      params.require(:plan).permit(:name, user_id)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.new
+user1.login = "joe"
+user1.email = "joe@cedarville.edu"
+user1.password = "password"
+user1.password_confirmation = "password"
+user1.save!
+
+user2 = User.new
+user2.login = "pete"
+user2.email = "pete@cedarville.edu"
+user2.password = "password"
+user2.password_confirmation = "password"
+user2.save!
+
+#give joe a couple of plans
+Plan.create(user_id: user1.id, name: "Plan1")
+Plan.create(user_id: user1.id, name: "Plan2")
